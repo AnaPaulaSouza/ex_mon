23e494b6b7c2ea337a5cabc44e5956288364cd81
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-03-07 12:46:57 -0300

    estrutura inicial

---
+++ lib/ex_mon_api_web/router.ex
@@ -0,0 +1,20 @@
+defmodule ExMonApiWeb.Router do
+  use ExMonApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", ExMonApiWeb do
+    pipe_through :api
+  end
+
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: ExMonApiWeb.Telemetry
+    end
+  end
+end
